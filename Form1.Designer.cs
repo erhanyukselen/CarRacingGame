446e53f74a81aa1c876e8e2b2e46dd5d13d1cc74
Erhan Ali Yükselen <erhanaly@gmail.com>
2024-11-17 01:34:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace CarRacingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pFerrari = new System.Windows.Forms.PictureBox();
+            this.pYesil = new System.Windows.Forms.PictureBox();
+            this.pTuruncu = new System.Windows.Forms.PictureBox();
+            this.pKirmizi = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pFerrari)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pYesil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pTuruncu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pKirmizi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(186, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 60);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(186, 107);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 60);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(186, 226);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 60);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(186, 351);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 60);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pFerrari
+            // 
+            this.pFerrari.Image = ((System.Drawing.Image)(resources.GetObject("pFerrari.Image")));
+            this.pFerrari.Location = new System.Drawing.Point(38, 319);
+            this.pFerrari.Name = "pFerrari";
+            this.pFerrari.Size = new System.Drawing.Size(45, 80);
+            this.pFerrari.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pFerrari.TabIndex = 4;
+            this.pFerrari.TabStop = false;
+            // 
+            // pYesil
+            // 
+            this.pYesil.Image = ((System.Drawing.Image)(resources.GetObject("pYesil.Image")));
+            this.pYesil.Location = new System.Drawing.Point(315, 319);
+            this.pYesil.Name = "pYesil";
+            this.pYesil.Size = new System.Drawing.Size(45, 80);
+            this.pYesil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pYesil.TabIndex = 5;
+            this.pYesil.TabStop = false;
+            // 
+            // pTuruncu
+            // 
+            this.pTuruncu.Image = ((System.Drawing.Image)(resources.GetObject("pTuruncu.Image")));
+            this.pTuruncu.Location = new System.Drawing.Point(227, 72);
+            this.pTuruncu.Name = "pTuruncu";
+            this.pTuruncu.Size = new System.Drawing.Size(45, 80);
+            this.pTuruncu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pTuruncu.TabIndex = 6;
+            this.pTuruncu.TabStop = false;
+            // 
+            // pKirmizi
+            // 
+            this.pKirmizi.Image = ((System.Drawing.Image)(resources.GetObject("pKirmizi.Image")));
+            this.pKirmizi.Location = new System.Drawing.Point(115, 139);
+            this.pKirmizi.Name = "pKirmizi";
+            this.pKirmizi.Size = new System.Drawing.Size(45, 80);
+            this.pKirmizi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pKirmizi.TabIndex = 7;
+            this.pKirmizi.TabStop = false;
+            this.pKirmizi.Click += new System.EventHandler(this.pKirmizi_Click);
+            // 
+            // p1
+            // 
+            this.p1.Image = ((System.Drawing.Image)(resources.GetObject("p1.Image")));
+            this.p1.Location = new System.Drawing.Point(73, 139);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(30, 30);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1.TabIndex = 8;
+            this.p1.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.Image = ((System.Drawing.Image)(resources.GetObject("p2.Image")));
+            this.p2.Location = new System.Drawing.Point(115, 72);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(30, 30);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2.TabIndex = 9;
+            this.p2.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.Image = ((System.Drawing.Image)(resources.GetObject("p3.Image")));
+            this.p3.Location = new System.Drawing.Point(227, 189);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(30, 30);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p3.TabIndex = 10;
+            this.p3.TabStop = false;
+            // 
+            // p4
+            // 
+            this.p4.Image = ((System.Drawing.Image)(resources.GetObject("p4.Image")));
+            this.p4.Location = new System.Drawing.Point(295, 72);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(30, 30);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p4.TabIndex = 11;
+            this.p4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Orange;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(108, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 37);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Game Over";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Orange;
+            this.label2.Location = new System.Drawing.Point(0, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Total Score=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Orange;
+            this.label3.Location = new System.Drawing.Point(111, -1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Orange;
+            this.label4.Location = new System.Drawing.Point(363, -1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Orange;
+            this.label5.Location = new System.Drawing.Point(252, -1);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "High Score=";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.button1.Location = new System.Drawing.Point(108, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 37);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Start Again";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(384, 411);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.pKirmizi);
+            this.Controls.Add(this.pTuruncu);
+            this.Controls.Add(this.pYesil);
+            this.Controls.Add(this.pFerrari);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pFerrari)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pYesil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pTuruncu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pKirmizi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pFerrari;
+        private System.Windows.Forms.PictureBox pYesil;
+        private System.Windows.Forms.PictureBox pTuruncu;
+        private System.Windows.Forms.PictureBox pKirmizi;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRacingGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible= false;
+            button1.Visible = false;
+        }
+
+        void Altınlar(int Hız)
+        {
+            if (p1.Top > 500)
+            {
+                x = r.Next(0, 200);
+                p1.Location = new Point(x, 0);
+            }
+            else
+            {
+                p1.Top += Hız;
+            }
+
+            if (p2.Top > 500)
+            {
+                x = r.Next(100, 300);
+                p2.Location = new Point(x, 0);
+            }
+            else
+            {
+                p2.Top += Hız;
+            }
+
+
+            if (p3.Top > 500)
+            {
+                x = r.Next(0, 350);
+                p3.Location = new Point(x, 0);
+            }
+            else
+            {
+                p3.Top += Hız;
+            }
+
+            if (p4.Top > 500)
+            {
+                x = r.Next(150, 350);
+                p4.Location = new Point(x, 0);
+            }
+            else
+            {
+                p4.Top += Hız;
+            }
+        }
+
+        int ToplamAltın = 0;
+        void ToplamAltınlar()
+        {
+            if (pFerrari.Bounds.IntersectsWith(p1.Bounds))
+            {
+                ToplamAltın++;
+                label3.Text = ToplamAltın.ToString();
+                x = r.Next(0, 350);
+                p1.Location=new Point(x, 0);
+            }
+            if (pFerrari.Bounds.IntersectsWith(p2.Bounds))
+            {
+                ToplamAltın++;
+                label3.Text = ToplamAltın.ToString();
+                x = r.Next(0, 350);
+                p2.Location = new Point(x, 0);
+            }
+            if (pFerrari.Bounds.IntersectsWith(p3.Bounds))
+            {
+                ToplamAltın++;
+                label3.Text = ToplamAltın.ToString();
+                x = r.Next(0, 350);
+                p3.Location = new Point(x, 0);
+            }
+            if (pFerrari.Bounds.IntersectsWith(p4.Bounds))
+            {
+                ToplamAltın++;
+                label3.Text = ToplamAltın.ToString();
+                x = r.Next(0, 350);
+                p4.Location = new Point(x, 0);
+            }
+        }
+        void OyunBitti()
+        {
+            if (pFerrari.Bounds.IntersectsWith(pKirmizi.Bounds))
+            {
+                label1.Visible = true;
+                button1.Visible=true;
+                timer1.Enabled = false;
+            }
+
+            if (pFerrari.Bounds.IntersectsWith(pTuruncu.Bounds))
+            {
+                label1.Visible = true;
+                button1.Visible = true;
+                timer1.Enabled = false;
+            }
+
+            if (pFerrari.Bounds.IntersectsWith(pYesil.Bounds))
+            {
+                label1.Visible = true;
+                button1.Visible = true;
+                timer1.Enabled = false;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //pictureBox1.Top += 5;
+            //pictureBox1.Top = pictureBox1.Top + 5;
+            cizgiHareketi(OyunHizi);
+            Araclar(OyunHizi);
+            OyunBitti();
+            Altınlar(OyunHizi);
+            ToplamAltınlar();
+
+        }
+        Random r = new Random();
+        int x;
+        void Araclar(int Hız)
+        {
+            if (pKirmizi.Top>500)
+            {
+               x=r.Next(0,200);
+               pKirmizi.Location= new Point(x,0);
+            }
+            else
+            {
+                pKirmizi.Top += Hız;
+            }
+            if (pTuruncu.Top > 500)
+            {
+                x = r.Next(0, 350);
+                pTuruncu.Location = new Point(x, 0);
+            }
+            else
+            {
+                pTuruncu.Top += Hız;
+            }
+
+            if (pYesil.Top > 500)
+            {
+                x = r.Next(200, 300);
+                pYesil.Location = new Point(x, 0);
+            }
+            else
+            {
+                pYesil.Top += Hız;
+            }
+        }
+        void cizgiHareketi(int Hiz)
+        {
+            if (pictureBox1.Top>450)
+            {
+                pictureBox1.Top = 0;
+            }
+            else
+            {
+                pictureBox1.Top += Hiz;
+            }
+
+            if (pictureBox2.Top > 450)
+            {
+                pictureBox2.Top = 0;
+            }
+            else
+            {
+                pictureBox2.Top += Hiz;
+            }
+
+            if (pictureBox3.Top > 450)
+            {
+                pictureBox3.Top = 0;
+            }
+            else
+            {
+                pictureBox3.Top += Hiz;
+            }
+
+            if (pictureBox4.Top > 450)
+            {
+                pictureBox4.Top = 0;
+            }
+            else
+            {
+                pictureBox4.Top += Hiz;
+            }
+        }
+        int OyunHizi = 0;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Right)
+            {
+                if (pFerrari.Right<380)
+                {
+                    pFerrari.Left += 8;
+                }
+            }
+            if (e.KeyCode==Keys.Left)
+            {
+                if (pFerrari.Left>0)
+                {
+                    pFerrari.Left += -8;
+                }
+            }
+
+            if (e.KeyCode==Keys.Up)
+            {
+                if (OyunHizi<21)
+                {
+                    OyunHizi++;
+                }
+            }
+
+            if (e.KeyCode==Keys.Down)
+            {
+                if (OyunHizi>0)
+                {
+                    OyunHizi--;
+                }
+            }
+        }
+
+        private void pKirmizi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
